16c031149ebcee2b5054313a6f1ece58ea981187
Hanmant panchal <123748781+Hanmantpanchal@users.noreply.github.com>
2026-07-10 09:12:51 +0530

    Delete Level_1/Day_1 directory

--- Level_1/Day_1/Day_2/circumference_circle.py
+++
@@ -1,9 +0,0 @@
-#11. Find the area and circumference of circle.
-
-radius = float(input("Enter the radius of the circle: "))
-
-area = 3.14 * radius ** 2
-circumference = 2 * 3.14 * radius
-
-print(f"Area of the circle is {area:.2f}")
-print(f"Circumference of the circle is {circumference:.2f}")
--- Level_1/Day_1/Q3.py
+++
@@ -1,9 +0,0 @@
-#3.	Find the largest of two numbers.
-num1=int(input("Enter a number: "))
-num2=int(input("Enter a number: "))
-if num1>num2:
-    print(f'{num1} is greater than {num2}')
-elif num2>num1:
-    print(f'{num2} is greater than {num1}')
-else:
-    print("Both are equal")
